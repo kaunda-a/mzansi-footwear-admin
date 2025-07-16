4f6e29beb1e202654fe4c7ceefd5bc2f9d8a4e0c
kaunda-a <kaunda-a@users.noreply.github.com>
2025-07-16 22:15:10 +0200

    Fix: Remove duplicate capitalizeSearchParam function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -50,11 +50,6 @@ export function capitalizeSearchParam(text: string) {
   return text.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
 }
 
-export function capitalizeSearchParam(text: string) {
-  if (!text) return "";
-  return text.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
-}
-
 interface Image {
   imagePublicId: string;
 }
